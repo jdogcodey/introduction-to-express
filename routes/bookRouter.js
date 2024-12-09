e03bd7c8422b8b19dc9bb24d387283433f7a4732
jdogcodey <jonnydawson@hotmail.com>
2024-12-09 17:00:15 +0000

    Write book Router module

---
+++ routes/bookRouter.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const bookRouter = Router();
+
+bookRouter.get("/", (req, res) => res.send("All books"));
+bookRouter.get("/:bookId", (req, res) => {
+  const { bookId } = req.params;
+  res.send(`Book ID: ${bookId}`);
+});
+bookRouter.get("/:booId/reserve", (req, res) => {
+  const { bookId } = req.params;
+  res.send(`Reserve Book ID: ${bookId}`);
+});
+bookRouter.post("/:bookId/reserve", (req, res) => {
+  const { bookId } = req.params;
+  res.send(`Reserve Post Book ID: ${bookId}`);
+});
+
+module.exports = bookRouter;
