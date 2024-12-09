2b8d663d370b638041466487927c6673a65e91e2
jdogcodey <jonnydawson@hotmail.com>
2024-12-09 20:24:31 +0000

    Following views tutorial

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const authorRouter = require("./routes/authorRoutes");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-app.use("/", indexRouter);
+app.use("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
 app.use((err, req, res, next) => {
   res.status(500).send(err);
 });
